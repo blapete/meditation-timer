d23b0f4475e14a2120f9067b9d4835c06c465a19
blapete <pjblank2@gmail.com>
2020-12-21 07:43:36 -0500

    timer file working, interval with 4 second messages

--- timer.js
+++ timer.js
@@ -8,3 +8,28 @@ function sleep(ms) {
         return setTimeout(resolve, ms);
     });
 }
+
+async function timer() {
+    console.log('Breathe in...');
+    while (true) {
+        let breathingInterval = setInterval(function () {
+            console.log('---------------');
+        }, 1000);
+        await sleep(4000);
+        y++;
+        clearInterval(breathingInterval);
+        if (y % 2 != 0) {
+            console.log('Hold...');
+        } else if (z % 2 != 0) {
+            console.log('Breathe out...');
+            z++;
+        } else {
+            x++;
+            console.log(`Breathe in...  | Cycles: ${x}`)
+            z++;
+        }
+
+    }
+}
+
+timer();
